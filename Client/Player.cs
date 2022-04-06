39c8500bff31910a435bfe379c865396040d26d4
Patrick S <68395365+Schapat@users.noreply.github.com>
2022-04-06 23:24:27 +0200

    Player.Class

---
+++ Client/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Player
+    {
+        public String playername;
+        public String playerRarPath;
+        public bool config;
+        public bool autoexec;
+        public bool video;
+
+
+        public Player(String playername, String playerCfgPath)
+        {
+        
+        }
+    }
+}
