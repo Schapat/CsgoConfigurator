655e1df9771ca05b5ee4a017765caf1078be7f78
Schapat <patrick.scharnow@gmx.de>
2022-04-13 22:15:48 +0200

    bla
    bla

---
+++ Backup/Data.cs
@@ -0,0 +1,14 @@
+﻿namespace Backup
+{
+    public class Data
+    {
+        public string name;
+        public string config;
+        public string autoexec;
+        public string video;
+
+        public Data()
+        {   
+        }
+    }
+}
